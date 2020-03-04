16e0f545f701fd62485cd6947e86688a73e0abe6
Nikilesh Subramoniapillai Ajeetha <niksubra@cisco.com>
2020-03-04 11:16:34 -0800

    Add aws msk endpoint logic

---
+++ templates/msk/mskendpoints/go.mod
@@ -0,0 +1,9 @@
+module msk
+
+go 1.13
+
+require (
+	github.com/aws/aws-lambda-go v1.14.0
+	github.com/aws/aws-sdk-go v1.29.10
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+)
---
+++ templates/msk/mskendpoints/mskendpoints.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/cfn"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambdacontext"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/kafka"
+	log "github.com/golang/glog"
+	"os"
+)
+
+var clusterarn = os.Getenv("clusterarn")
+var kafkaConnString, zookeeperConnStr string
+
+func mskEndPoints(ctx context.Context, event cfn.Event) (physicalResourceID string, data map[string]interface{}, err error) {
+
+	event.ResourceProperties["PhysicalResourceID"] = lambdacontext.LogStreamName
+
+	data = map[string]interface{}{}
+
+	if event.RequestType == "Create" {
+
+		describeclusterinputarn := &kafka.DescribeClusterInput{
+			ClusterArn: aws.String(clusterarn),
+		}
+		getBootstrapBrokersInput := &kafka.GetBootstrapBrokersInput{
+			ClusterArn: aws.String(clusterarn),
+		}
+		mySession := session.Must(session.NewSession())
+		svc := kafka.New(mySession)
+
+		if describeclusteroutput, err := svc.DescribeCluster(describeclusterinputarn); err != nil {
+			log.Errorf("Failed to describe cluster: %v", err)
+		} else {
+
+			zookeeperConnStr = *describeclusteroutput.ClusterInfo.ZookeeperConnectString
+		}
+		if getBootstrapBrokersOutput, err := svc.GetBootstrapBrokers(getBootstrapBrokersInput); err != nil {
+			log.Errorf("Failed to Bootstrapbroker cluster: %v", err)
+		} else {
+			kafkaConnString = getBootstrapBrokersOutput.GoString()
+		}
+		data["ConnectionString"] = "ZookeeperConnStr: " + zookeeperConnStr + " " + kafkaConnString
+	}
+	return
+}
+
+func main() {
+	lambda.Start(cfn.LambdaWrap(mskEndPoints))
+}
